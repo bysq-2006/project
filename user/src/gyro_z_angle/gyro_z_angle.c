0891ab0cb0a8226b09c069d987336177915633e3
bysq <1904568840@qq.com>
2026-06-02 11:20:08 +0800

    优化

--- user/src/gyro_z_angle/gyro_z_angle.c
+++ user/src/gyro_z_angle/gyro_z_angle.c
@@ -1,12 +1,9 @@
 /*********************************************************************************************************************
-* gyro_z 原始值积分与缩放输出
+* gyro_z raw integration and scaled output
 *********************************************************************************************************************/
 
 #include "gyro_z_angle.h"
-#include "../screen_print/screen_print.h"
-#include <stdio.h>
 
-#define GYRO_Z_PRINT_BUFFER_SIZE    (32)
 #define GYRO_Z_DEAD_ZONE_RAW        (6)
 #define GYRO_Z_OUTPUT_SCALE         (0.002f)
 #define GYRO_Z_INTEGRAL_LIMIT_RAW   (2137500)
@@ -14,18 +11,6 @@
 static int16 gyro_z_raw;
 static int32 gyro_z_integral;
 static float gyro_z_output;
-static int8 gyro_z_control_w;
-static char gyro_z_print_buffer[GYRO_Z_PRINT_BUFFER_SIZE];
-
-static int32 float_to_milli_int(float value)
-{
-    if(value >= 0.0f)
-    {
-        return (int32)(value * 1000.0f + 0.5f);
-    }
-
-    return (int32)(value * 1000.0f - 0.5f);
-}
 
 static int32 gyro_z_limit_integral(int32 value)
 {
@@ -47,21 +32,16 @@ void gyro_z_angle_reset(void)
     gyro_z_raw = 0;
     gyro_z_integral = 0;
     gyro_z_output = 0.0f;
-    gyro_z_control_w = 0;
 }
 
 void gyro_z_angle_init(void)
 {
     gyro_z_angle_reset();
-    screen_print_init();
-    screen_print_line(0, "gyro_z raw");
 }
 
 void gyro_z_angle_update(uint16 dt_ms)
 {
     int16 gyro_z_calc;
-    int32 output_milli;
-    char output_sign = ' ';
 
     imu660rb_get_gyro();
     gyro_z_raw = imu660rb_gyro_z;
@@ -69,28 +49,11 @@ void gyro_z_angle_update(uint16 dt_ms)
     gyro_z_integral += (int32)gyro_z_calc * dt_ms;
     gyro_z_integral = gyro_z_limit_integral(gyro_z_integral);
     gyro_z_output = (float)gyro_z_integral * GYRO_Z_OUTPUT_SCALE;
-
-    screen_print_line(0, "IMU660RB gyro raw");
-    sprintf(gyro_z_print_buffer, "gyro_z:%6d", gyro_z_raw);
-    screen_print_line(1, gyro_z_print_buffer);
-    sprintf(gyro_z_print_buffer, "int_z:%ld", (long)gyro_z_integral);
-    screen_print_line(2, gyro_z_print_buffer);
-
-    output_milli = float_to_milli_int(gyro_z_output);
-    if(output_milli < 0)
-    {
-        output_sign = '-';
-        output_milli = -output_milli;
-    }
-    sprintf(gyro_z_print_buffer, "out_z:%c%ld.%03ld", output_sign, (long)(output_milli / 1000), (long)(output_milli % 1000));
-    screen_print_line(3, gyro_z_print_buffer);
-    sprintf(gyro_z_print_buffer, "w:%4d", gyro_z_control_w);
-    screen_print_line(4, gyro_z_print_buffer);
 }
 
 void gyro_z_angle_set_w(int8 w)
 {
-    gyro_z_control_w = w;
+    (void)w;
 }
 
 int16 gyro_z_angle_get_raw(void)
--- user/src/gyro_z_angle/gyro_z_angle.h
+++ user/src/gyro_z_angle/gyro_z_angle.h
@@ -1,18 +1,27 @@
-/*********************************************************************************************************************
-* gyro_z 原始值积分与缩放输出
-*********************************************************************************************************************/
+/* gyro_z 角度积分与缩放输出接口 */
 
+/* 防止头文件重复包含 */
 #ifndef _gyro_z_angle_h_
+/* 头文件保护宏开始 */
 #define _gyro_z_angle_h_
 
+/* 引入基础数据类型定义 */
 #include "zf_common_headfile.h"
 
+/* 初始化陀螺仪 Z 轴角度模块 */
 void gyro_z_angle_init(void);
+/* 重置陀螺仪 Z 轴角度模块状态 */
 void gyro_z_angle_reset(void);
+/* 更新陀螺仪 Z 轴角度积分结果 */
 void gyro_z_angle_update(uint16 dt_ms);
+/* 设置当前控制量 w */
 void gyro_z_angle_set_w(int8 w);
+/* 获取陀螺仪 Z 轴原始值 */
 int16 gyro_z_angle_get_raw(void);
+/* 获取陀螺仪 Z 轴积分值 */
 int32 gyro_z_angle_get_integral(void);
+/* 获取陀螺仪 Z 轴角度输出值 */
 float gyro_z_angle_get_output(void);
 
+/* 头文件保护宏结束 */
 #endif
